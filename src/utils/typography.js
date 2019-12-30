a2c4e6e16303ae4f796df76097b720f212940082
Earl Lee <earlvlee@gmail.com>
2019-12-29 21:31:33 -0500

    Decrease font size

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,9 @@ import Typography from "typography";
 import kirkhamTheme from "typography-theme-kirkham";
 
 kirkhamTheme.overrideStyles = ({ rhythm }, options) => ({
+  body: {
+    fontSize: "0.85em"
+  },
   a: {
     textDecoration: "none",
     // color: "#386dc0" // Blue
