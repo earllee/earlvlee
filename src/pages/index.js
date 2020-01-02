2a0a8490bf6c3efac7982e4506aa5b7ed7e2bf9c
Earl Lee <earlvlee@gmail.com>
2020-01-02 12:06:51 -0500

    Fix broken home page link to 2019 Reflection

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default ({ data }) => (
     </Helmet>
     <img src="../earl-in-rome.jpg" alt="Earl in Rome" />
     <p className="latest-post-container">
-      Read my latest blog post <a href="/2020">here</a>!
+      Read my latest blog post <a href="/2019-reflection">here</a>!
     </p>
     <p>
       Hi, my name is Earl Lee. I'm an MS/MBA student at{" "}
