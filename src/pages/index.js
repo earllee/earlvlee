e40c6e956e2ea6b75a5bf62a33dfcea411faa287
Earl Lee <earlvlee@gmail.com>
2020-05-09 02:02:04 -0400

    Update bio for summer 2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,9 @@ export default ({ data }) => (
       src="../reflection-in-mirror.jpg"
       alt="Reflection in mirror of Earl Lee"
     />
-    <p className="latest-post-container">
-      Read my latest blog post{" "}
-      <a href="/covid-19-podcast-arthur-brooks-len-schlesinger">here</a>!
-    </p>
+    <h3>Hi, my name is Earl</h3>
     <p>
-      Hi, my name is Earl Lee. I'm an MS/MBA student at{" "}
+      I'm an MS/MBA student at{" "}
       <OutboundLink
         target="_blank"
         href="http://www.hbs.edu/ms-mba"
@@ -34,7 +31,7 @@ export default ({ data }) => (
       >
         School of Engineering and Applied Sciences
       </OutboundLink>
-      . On the side, I consult for{" "}
+      . Before school, I was an investment fellow at{' '}
       <OutboundLink
         target="_blank"
         href="http://www.costanoavc.com"
@@ -42,7 +39,7 @@ export default ({ data }) => (
       >
         Costanoa Ventures
       </OutboundLink>
-      , where I was an early-stage B2B investment fellow before starting at HBS.
+      , a Seed & Series A investment firm.
     </p>
     <p>
       Previously, I helped start{" "}
@@ -54,7 +51,7 @@ export default ({ data }) => (
         FiscalNote
       </OutboundLink>
       , a B2B SaaS company that helps organizations ranging from Fortune 500
-      companies to nonprofits manage change in government policy by using data
+      companies to nonprofits deal with change in government policy by using data
       and machine learning. I joined as the 3rd employee and helped us grow to
       400+ employees, serving as a software engineer for the first two years and
       product manager during the last three. By the end, I led a
@@ -81,10 +78,9 @@ export default ({ data }) => (
       .
     </p>
 
-    <h3>Side-Project</h3>
-    <p>Currently working on exploring ideas in the fitness space.</p>
-    <p>
-      My latest side project is{" "}
+    <h3>Currently</h3>
+    <p>I'm building something new and reading a lot of books. On the side, I
+      work on{' '}
       <OutboundLink
         target="_blank"
         href="https://retaino.com"
@@ -94,12 +90,12 @@ export default ({ data }) => (
       </OutboundLink>
       , a way to overcome the mindless consumption of content driven by ads and
       algorithms and, instead, focus on learning and retaining useful knowledge
-      using spaced repetition.
+      across the web using spaced repetition.
     </p>
 
     <h3>Interests</h3>
     <p>
-      I am an avid{" "}
+      I'm an avid{" "}
       <OutboundLink
         target="_blank"
         href="https://instagram.com/earllifts"
@@ -115,7 +111,10 @@ export default ({ data }) => (
       >
         photographer
       </OutboundLink>
-      .
+      . One of my favorite weekend activies is to post up at a coffee shop and
+      read or write code. In the absence of a coffee shop, I like to make my
+      own pourovers. Some of the online writers I like include Ben Thompson,
+      Byrne Hobart, Matt Levine, Alex Danco, Nathan Baschez, and Eugene Wei.
     </p>
 
     <div className="social-icons-container">
